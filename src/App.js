cfbf82b68db31b2c38f31ddbad5f7fa49148d827
Mikhail <zulwaran@gmail.com>
2021-09-30 08:23:04 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import MainContainer from './components/MainPage/MainContainer'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import thunk from 'redux-thunk'
+import FavoriteContainer from './components/FavoritePage/FavoriteContainer';
+import styles from './App.css'
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
 const App = () => {
   return (
     <Provider store={store}>
-      <MainContainer />
+      <Router>
+        <div>
+          <nav className='navigation'>
+            <ul>
+              <li>
+                <Link to="/">Главная страница</Link>
+              </li>
+              <li>
+                <Link to="/favorite">Любимые герои</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/favorite">
+              <FavoriteContainer />
+            </Route>
+            <Route path="/">
+              <MainContainer />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   )
 }
---
+++ src/components/FavoritePage/FavoriteContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FavoriteContainer = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default FavoriteContainer
